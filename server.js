7f87927b17fcdac05a21b7e477f62f39f00a2616
Colin Stell <ColinStellDev96@users.noreply.github.com>
2017-08-29 15:23:59 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('./public'));
+
+
+app.use(function(req, res, next) {
+    res.status(404).send('not found');
+});
+app.use(function(req, res, next) {
+    res.status(500).send('oops');
+});
+app.listen(8000);
